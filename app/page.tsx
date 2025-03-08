7b5dd090f4ea7840ab773a5af582eab865ceb362
Arjun-1807 <arjun2005gowda@gmail.com>
2025-03-08 19:56:38 +0530

    sign-in-up page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="center">
+    <div className="flex-center h-screen">
       <h1 className="text-3xl text-brand font-bold">
         StoreIt - The only storage solution you need.
       </h1>
